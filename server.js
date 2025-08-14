d1c606c9e25785cfa373bf01116d7fd847530a56
faizan ahmad mir <faizan.mir@acmo.in>
2025-08-14 20:33:48 +0530

    database configuration done

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app  = require('./app.js');
 require('dotenv').config();
-const db = process.env.DB_NAME;
+const connectDB = require("./config/db.js");    
+
+  connectDB().then(()=>{
+    console.log("DB connected");       
+  }).catch((err)=>{
+    console.log("Error occured while connecting to database");
+  })  
+
+
+
+
 const portNumber =  process.env.PORT_NUMBER || 3000;
 // console.log(db)
 app.listen(portNumber,()=>{
