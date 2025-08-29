751f177a11b038524698ee6ee2181de90199c702
faizan ahmad mir <faizan.mir@acmo.in>
2025-08-29 11:49:31 +0530

    admin routes initialised

--- routes/api/v1/Admin.routes.js
+++ routes/api/v1/Admin.routes.js
@@ -1,5 +1,11 @@
 const express  = require("express");
-const router = express.Router();  
+const router = express.Router(); 
+const authenticateJWT = require("../../../helpers/customerauthentication.js"); 
+const { addCategory } = require("../../../controllers/api/v1/admin/Categories.js");
+
+
+// category routes:
+router.post("/add-category",authenticateJWT,addCategory);
 
 
 module.exports = router;
