56951216ea5f7228841ff9dc350ec5dd6f04c903
faizan ahmad mir <faizan.mir@acmo.in>
2025-08-31 12:43:33 +0530

    add product controller and its route created

--- routes/api/v1/Admin.routes.js
+++ routes/api/v1/Admin.routes.js
@@ -2,6 +2,7 @@ const express  = require("express");
 const router = express.Router(); 
 const authenticateJWT = require("../../../helpers/adminauthentication.js"); 
 const { addCategory,updateCategory,fetchCategory,fetchAllcategories,deleteCategory} = require("../../../controllers/api/v1/admin/Categories.js");
+const { addProduct } = require("../../../controllers/api/v1/admin/Products.js");
 
 
 // category routes:
@@ -10,5 +11,9 @@ router.put('/update-category',authenticateJWT,updateCategory);
 router.get('/category',authenticateJWT,fetchCategory)
 router.get('/categories',authenticateJWT,fetchAllcategories);
 router.delete('/delete-category',authenticateJWT,deleteCategory);
+
+
+// Product routes:
+router.post('/add-product',authenticateJWT,addProduct);
 
 module.exports = router;
