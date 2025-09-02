5e6f72b9f922f7805d75640fb79d936a5bd175ef
faizan ahmad mir <faizan.mir@acmo.in>
2025-09-02 19:46:35 +0530

    cart routes created

--- routes/api/v1/Customer.routes.js
+++ routes/api/v1/Customer.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const {signUpUser, loginUser} = require("../../../controllers/api/v1/customer/Login.js");
 const { createProfile, updateProfile, fetchProfile } = require("../../../controllers/api/v1/customer/customer.js");
-
+const authenticateJWT = require("../../../helpers/customerauthentication.js");
+const { addToCart, deleteProductFromCart } = require("../../../controllers/api/v1/customer/Cart.js");
 
 // login || signup routes:
 router.post("/login",loginUser);
@@ -13,6 +14,10 @@ router.post('/user/signup',signUpUser);
 router.post("/add-profile/:u_id",createProfile);
 router.put("/update-profile/:u_id",updateProfile);
 router.get('/profile/:u_id',fetchProfile);
+
+// cart routes:
+router.post("/add-cart",authenticateJWT,addToCart);
+router.delete("/remove-cart",authenticateJWT,deleteProductFromCart);
 
 
 
